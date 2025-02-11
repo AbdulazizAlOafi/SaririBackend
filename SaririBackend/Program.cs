54771e901304d8e803da8cff77c20e2507ede15a
AbdulazizAlOafi <149675702+AbdulazizAlOafi@users.noreply.github.com>
2025-02-11 12:51:00 +0300

    Update Program.cs

--- SaririBackend/Program.cs
+++ SaririBackend/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using SaririBackend.Data;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add PostgreSQL Database Connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
